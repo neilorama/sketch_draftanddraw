7b8f05562f6a3c7a8818960f89add8fd25826f71
neilorama <neil@neilmullins.com>
2014-02-05 20:28:34 -0500

    adding in the resize directive and setting up the base html

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,34 @@
 'use strict';
 
-angular.module('sketchDraftanddrawApp')
+var app = angular.module('sketchDraftanddrawApp')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+  })
+  .directive('resize', function ($window){
+    return function(scope, element) {
+        var w = angular.element($window);
+        scope.getWindowDimensions = function(){
+            return { 'h' :w.height(), 'w': w.width() };
+        };
+        scope.$watch(scope.getWindowDimensions, function( newValue, oldValue ){
+            scope.windowHeight = newValue.h;
+            scope.windowWidth  = newValue.w;
+
+            scope.style = function(){
+                return {
+                    'height': newValue.h,
+                    'width' : newValue.w
+                };
+            };
+        }, true);
+
+        w.bind('resize', function() {
+            scope.$apply();
+        })
+    }
+
   });
