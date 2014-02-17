c2bbc616da5cde6b631772ec79f750649a023819
neilorama <neil@neilmullins.com>
2014-02-17 16:21:34 -0500

    drawing config work

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,34 +1,93 @@
 'use strict';
 
-var app = angular.module('sketchDraftanddrawApp')
-  .controller('MainCtrl', function ($scope) {
+//setting up the base app module to extend off
+var app = angular.module('sketchDraftanddrawApp');
+
+// Main controller for the controls
+app.controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  })
-  .directive('resize', function ($window){
-    return function(scope, element) {
-        var w = angular.element($window);
-        scope.getWindowDimensions = function(){
-            return { 'h' :w.height(), 'w': w.width() };
-        };
-        scope.$watch(scope.getWindowDimensions, function( newValue, oldValue ){
-            scope.windowHeight = newValue.h;
-            scope.windowWidth  = newValue.w;
 
-            scope.style = function(){
-                return {
-                    'height': newValue.h,
-                    'width' : newValue.w
-                };
-            };
-        }, true);
+    // Instanciating up the sketch canvas with specific values
+    var color    = '#ffffff',
+    radius       = 0,
+    transparity  = '',
+    stroke       = '',
+    drawingSurfaceImageData = '',
+    data = '',
+    sketchCanvas = Sketch.create({
+        container: document.getElementById( 'container' ),
+        autoclear: false,
 
-        w.bind('resize', function() {
-            scope.$apply();
-        })
+        setup: function() {
+            console.log( 'setup' );
+        },
+
+        // update: function() {
+        //     radius = 2 + abs( sin( this.millis * 0.003 ) * 50 );
+        // },
+
+        // Event handlers
+
+        keydown: function() {
+            if ( this.keys.C ) this.clear();
+        },
+
+        // Mouse & touch events are merged, so handling touch events by default
+        // and powering sketches using the touches array is recommended for easy
+        // scalability. If you only need to handle the mouse / desktop browsers,
+        // use the 0th touch element and you get wider device support for free.
+        touchmove: function() {
+
+            for ( var i = this.touches.length - 1, touch; i >= 0; i-- ) {
+
+                touch = this.touches[i];
+
+                this.lineCap = 'round';
+                this.lineJoin = 'round';
+                this.fillStyle = this.strokeStyle = color;
+                this.lineWidth = radius;
+
+                this.beginPath();
+                this.moveTo( touch.ox, touch.oy );
+                this.lineTo( touch.x, touch.y );
+                this.stroke();
+            }
+        }
+    });
+    // controls to be refactored, possibly a factory but have to look into scope issues
+    $scope.controls = function(type, attr){
+        if ( type == 'radius'){
+            radius = attr;
+        } else if ( type == 'color' ) {
+            color = attr;
+        } else if ( type == 'eraser' ) {
+            transparity = sketchCanvas.globalCompositeOperation;
+            stroke      = sketchCanvas.strokeStyle;
+            sketchCanvas.globalCompositeOperation = "destination-out";
+            sketchCanvas.strokeStyle = "rgba(0,0,0,1)";
+        } else if ( type == 'pencil' ){
+            sketchCanvas.globalCompositeOperation = transparity;
+            sketchCanvas.strokeStyle = stroke;
+        } else if ( type == 'save' ){
+            drawingSurfaceImageData = sketchCanvas.getImageData(0, 0,
+                             sketchCanvas.width,
+                             sketchCanvas.height);
+            data = sketchCanvas.canvas.toDataURL();
+            window.open(data, "toDataURL() image", "width=700, height=900");
+        } else if ( type == 'draw' ){
+            sketchCanvas.putImageData(drawingSurfaceImageData, 0, 0);
+        } else if ( type == 'clear' ){
+            sketchCanvas.clearRect(0, 0, sketchCanvas.width, sketchCanvas.height);
+        }
     }
+});
 
-  });
+
+
+
+
+// app.directive('')
